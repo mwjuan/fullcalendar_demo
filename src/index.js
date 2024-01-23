016ece83c2901ee5ca550c11b3057da86f003759
mwjuan <50393260+mwjuan@users.noreply.github.com>
2024-01-23 17:46:22 +0800

    fix branch

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom";
-
-import ReactBigCalendar from "./ReactBigCalendar";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <ReactBigCalendar />,
-  rootElement
-);
