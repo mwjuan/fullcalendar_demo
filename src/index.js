15f8c4b3646e4568c092ea7e516a85f6d8af7ce8
mwj <mawenjuan@shgbit.com>
2024-01-23 14:22:52 +0800

    init: 🎉

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+
+import ReactBigCalendar from "./ReactBigCalendar";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <ReactBigCalendar />,
+  rootElement
+);
